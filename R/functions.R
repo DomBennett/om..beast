70fce195e2f15ae7405515a3004381e0e7535286
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-05-09 20:53:06 +0200

    updates for new packages

--- R/functions.R
+++ R/functions.R
@@ -5,13 +5,11 @@
 #' @example /examples/example.R
 #' @export
 beast <- function(...) {
-  arglist <- outsider:::.arglist_get(...)
-  files_to_send <- outsider::.filestosend_get(arglist = arglist)
-  arglist <- outsider::.arglist_parse(arglist = arglist)
-  otsdr <- outsider::.outsider_init(repo = 'dombennett/om..beast',
-                                    cmd = 'beast', wd = getwd(),
-                                    arglist = arglist,
-                                    files_to_send = files_to_send)
+  arglist <- arglist_get(...)
+  files_to_send <- filestosend_get(arglist = arglist)
+  arglist <- arglist_parse(arglist = arglist)
+  otsdr <- outsider_init(pkgnm = 'om..beast', cmd = 'beast', wd = getwd(),
+                         arglist = arglist, files_to_send = files_to_send)
   # run the command
-  outsider::.run(otsdr)
+  run(otsdr)
 }
